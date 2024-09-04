fd28c2e980b4436eabb3a102f3301b728bc2a3d6
Reece <1295273096@qq.com>
2024-09-04 12:02:58 +0800

    demo

---
+++ utils/categories.py
@@ -0,0 +1,106 @@
+
+
+Categories={
+    # new added
+    'LEISI_V2':[
+        "leisi_norm", # 0
+        # 'LEISI_V2'
+    ],
+    'VisA':[
+        "candle",  # 0
+        "capsules",  # 1
+        "cashew",  # 2
+        "chewinggum",  # 3
+        "fryum",  # 4
+        "macaroni1",  # 5
+        "macaroni2",  # 6
+        "pcb1",  # 7
+        "pcb2",  # 8
+        "pcb3",  # 9
+        "pcb4",  # 10
+        "pipe_fryum",  # 11
+    ],
+    'MVTec-AD':[
+        "bottle",  # 0
+        "cable",  # 1
+        "capsule",  # 2
+        "carpet",  # 3
+        "grid",  # 4
+        "hazelnut",  # 5
+        "leather",  # 6
+        "metal_nut",  # 7
+        "pill",  # 8
+        "screw",  # 9
+        "tile",  # 10
+        "toothbrush",  # 11
+        "transistor",  # 12
+        "wood",  # 13
+        "zipper",  # 14
+    ],
+    'MPDD':[
+        "bracket_black",  # 0
+        "bracket_brown",  # 1
+        "bracket_white",  # 2
+        "connector",  # 3
+        "metal_plate",  # 4
+        "tubes",  # 5
+    ],
+    'BTAD':[
+        "01",  # 0
+        "02",  # 1
+        "03",  # 2
+    ]
+}
+
+
+CategoriesDict={
+    'LEISI_V2':{
+        'leisi_norm':0,
+        # 'LEISI_V2': 0,
+    },
+    'VisA':{
+        "candle":0,
+        "capsules":1,
+        "cashew":2,
+        "chewinggum":3,
+        "fryum":4,
+        "macaroni1":5,
+        "macaroni2":6,
+        "pcb1":7,
+        "pcb2":8,
+        "pcb3":9,
+        "pcb4":10,
+        "pipe_fryum":11,
+    },
+    'MVTec-AD':{
+        "bottle": 0,
+        "cable": 1,
+        "capsule": 2,
+        "carpet": 3,
+        "grid": 4,
+        "hazelnut": 5,
+        "leather": 6,
+        "metal_nut": 7,
+        "pill": 8,
+        "screw": 9,
+        "tile": 10,
+        "toothbrush": 11,
+        "transistor": 12,
+        "wood": 13,
+        "zipper": 14,
+    },
+    'MPDD':{
+        "bracket_black":0,
+        "bracket_brown":1,
+        "bracket_white":2,
+        "connector":3,
+        "metal_plate":4,
+        "tubes":5,
+    },
+    'BTAD':{
+        "01":0,
+        "02":1,
+        "03":2,
+    }
+}
+
